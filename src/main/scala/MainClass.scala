5ecfc376b26cd86ae49f7755b3e166542f81f470
Tom Liddle <tom.liddle@gmail.com>
2015-11-24 17:08:11 +0000

    Scala template

---
+++ src/main/scala/MainClass.scala
@@ -0,0 +1,16 @@
+
+
+object MainClass {
+	def main(args: Array[String]): Unit = {
+		val mainClass = new MainClass
+		mainClass.run
+	}
+}
+
+class MainClass {
+
+	def run = {
+
+	}
+
+}
---
+++ src/test/scala/MainClassTest.scala
@@ -0,0 +1,20 @@
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.Matchers
+import org.scalatest.WordSpec
+
+class MainClassTest extends WordSpec with Matchers with BeforeAndAfterEach{
+
+	val shoppingCart = new MainClass
+
+	"MainClass" when {
+
+		"running" should {
+
+			"run correctly" in {
+
+			}
+
+		}
+	}
+}
